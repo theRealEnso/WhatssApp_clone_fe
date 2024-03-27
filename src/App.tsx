edaa84845bf4962665c54abbffe186e53f10c300
Ben <bennnnnnnvu@gmail.com>
2024-03-26 20:01:38 -0700

    create new vite react-app + typescript. Installed and configured tailwindcss

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './index.css';
+
+const  App = () => {
+
+  return (
+    <>
+      <div>
+        <h1>Welcome to our react app front end!</h1>
+      </div>
+    </>
+  )
+}
+
+export default App;
---
+++ src/svg/index.tsx
@@ -0,0 +1,57 @@
+import CommunityIcon from "./CommunityIcon.tsx";
+import StoryIcon from "./StoryIcon.tsx";
+import ChatIcon from "./ChatIcon.tsx";
+import NotificationIcon from "./NotificationIcon.tsx";
+import ArrowIcon from "./ArrowIcon.tsx";
+import CloseIcon from "./CloseIcon.tsx";
+import SearchIcon from "./SearchIcon.tsx";
+import DotsIcon from "./DotsIcon.tsx";
+import FilterIcon from "./FilterIcon.tsx";
+import SeenIcon from "./SeenIcon.tsx";
+import Logo from "./LogoIcon.tsx";
+import ReturnIcon from "./ReturnIcon.tsx";
+import SearchLargeIcon from "./SearchLargeIcon.tsx";
+import EmojiIcon from "./EmojiIcon.tsx";
+import AttachmentIcon from "./AttachmentIcon.tsx";
+import SendIcon from "./SendIcon.tsx";
+import PhotoIcon from "./PhotoIcon.tsx";
+import StickerIcon from "./StickerIcon.tsx";
+import CameraIcon from "./CameraIcon.tsx";
+import DocumentIcon from "./DocumentIcon.tsx";
+import ContactIcon from "./ContactIcon.tsx";
+import PollIcon from "./PollIcon.tsx";
+import SmallArrowIcon from "./SmallArrowIcon.tsx";
+import LockIcon from "./LockIcon.tsx";
+import CallIcon from "./CallIcon.tsx";
+import MuteIcon from "./MuteIcon.tsx";
+import NoVideoIcon from "./NoVideoIcon.tsx";
+
+export {
+  CommunityIcon,
+  StoryIcon,
+  ChatIcon,
+  NotificationIcon,
+  ArrowIcon,
+  CloseIcon,
+  SearchIcon,
+  DotsIcon,
+  FilterIcon,
+  SeenIcon,
+  Logo,
+  ReturnIcon,
+  SearchLargeIcon,
+  EmojiIcon,
+  AttachmentIcon,
+  SendIcon,
+  PhotoIcon,
+  StickerIcon,
+  CameraIcon,
+  DocumentIcon,
+  ContactIcon,
+  PollIcon,
+  SmallArrowIcon,
+  LockIcon,
+  CallIcon,
+  MuteIcon,
+  NoVideoIcon,
+};
