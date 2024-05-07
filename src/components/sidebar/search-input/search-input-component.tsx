46efd3b90e958bd7d0180ef8ebd2a3c98da875f2
Ben <bennnnnnnvu@gmail.com>
2024-05-07 16:30:31 -0700

    conditionally render the return arrow icon depending on when input is focused or not. Added animation styling to rotate the icon on focus

--- src/components/sidebar/search-input/search-input-component.tsx
+++ src/components/sidebar/search-input/search-input-component.tsx
@@ -1,15 +1,33 @@
+import { useState } from "react";
+
 import axios from "axios";
 import { SearchIcon, ReturnIcon, FilterIcon } from "../../../svg";
 
 export const SearchInput = () => {
+  const [returnIcon, setReturnIcon] = useState(false);
+
+  const showReturnIcon = () => setReturnIcon(true);
+  const hideReturnIcon = () => setReturnIcon(false);
   return (
     <div className="w-full flex items-center justify-center">
-        <div className="h-[45px] flex items-center dark:bg-dark_bg_2 rounded-lg">
-            <SearchIcon className="dark:fill-dark_svg_2 dark:bg_dark_bg_2 mx-2"></SearchIcon>
+        <div className="h-[50px] flex items-center dark:bg-dark_bg_2 rounded-lg">
+          {
+            returnIcon 
+              ? <div className="flex items-center justify-center px-2 rotateAnimation cursor-pointer">
+                  <ReturnIcon className="dark:fill-green_1 h-full w-full"></ReturnIcon>
+                </div>
+              : <div className="flex items-center justify-center px-2">
+                  <SearchIcon className="dark:fill-dark_svg_2 dark:bg_dark_bg_2 h-full w-full"></SearchIcon>
+                </div>
+          }
+
             <input 
                 type="text" 
                 placeholder="Search or start a new chat..." 
-                className="h-[40px] rounded-lg px-2 dark:bg-dark_bg_2 outline-none text-white">
+                className="h-[40px] rounded-lg px-2 dark:bg-dark_bg_2 outline-none text-white"
+                onFocus={showReturnIcon}
+                onBlur={hideReturnIcon}
+                >
             </input>
         </div>
         
