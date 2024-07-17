f4bd86d6e4910547906535753208009a196e3778
Ben <bennnnnnnvu@gmail.com>
2024-07-16 21:57:26 -0700

    begin implementing pagination for the mini preview selector + mini preview component

--- src/components/chat-window/preview/files/mini-preview-selector.tsx
+++ src/components/chat-window/preview/files/mini-preview-selector.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { useSelector } from "react-redux";
 
 // import selectors
@@ -12,10 +13,32 @@ import { MiniPreview } from "./mini-preview-component";
 // import socket context to convert component to one that has access to the socket
 
 export const MiniPreviewSelector = ({activeIndex, setActiveIndex, message}) => {
+  const [currentPage, setCurrentPage] = useState<number>(0);
 
   const files = useSelector(selectFiles);
 
   console.log(files);
+
+  const itemsPerPage = 5;
+  const totalPages = Math.ceil(files.length / itemsPerPage);
+
+  const nextPage = () => {
+    if(currentPage <= totalPages - 1){
+      setCurrentPage(currentPage + 1);
+    }
+  };
+
+  const previousPage = () => {
+    if(currentPage > 0){
+      setCurrentPage(currentPage - 1)
+    }
+  };
+
+  const startingIndex = currentPage * itemsPerPage;
+
+  const filesToDisplay = files.slice(startingIndex,  startingIndex + itemsPerPage);
+
+  console.log(currentPage);
 
   // solution may work, but is not a best practice. It is recommended to keep logic that handles state management + updates directly in the reducer
   // const removeSelectedFile = (index: number) => {
@@ -24,22 +47,44 @@ export const MiniPreviewSelector = ({activeIndex, setActiveIndex, message}) => {
   // };
 
   return (
-    <div className="flex space-x-2">
+    <div className="flex">
+      <div className="flex items-center justify-center space-x-10">
+        <div>
+          <button onClick={previousPage}>
+            <div className="flex flex-col">
+              <span>previous</span>
+              <span>{`<---`}</span>
+            </div>
+          </button>
+        </div>
 
-      {/* list and display files as thumbnails */}
-      
-      {
-        files && files.map((file, index) => {
-          // console.log(file);
-          return (
-            <MiniPreview file={file} index={index} activeIndex={activeIndex} setActiveIndex={setActiveIndex}></MiniPreview>
-          )
-        })
-      }
-     
+        {/* list and display files as thumbnails */}
+        
+        {
+          files && files.map((file, index) => {
+            // console.log(file);
+            return (
+              <MiniPreview file={file} index={index} activeIndex={activeIndex} setActiveIndex={setActiveIndex}></MiniPreview>
+            )
+          })
+        }
 
-      {/* Add another file */}
-      <Add></Add>
+        <div>
+          <button onClick={nextPage}>
+            <div className="flex flex-col">
+              <span>next</span>
+              <span>{`--->`}</span>
+            </div>
+          </button>
+        </div>
+      </div>
+
+      <div className="relative left-[40px]">
+        {/* Add another file */}
+        <Add></Add>
+      </div>     
+
+
     </div>
   );
 };
