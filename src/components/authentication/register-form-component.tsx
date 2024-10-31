d37fc9f138d9d1d7b193625537deff4a68a467eb
Ben <bennnnnnnvu@gmail.com>
2024-10-30 21:06:59 -0700

    change name of button on the register form

--- src/components/authentication/register-form-component.tsx
+++ src/components/authentication/register-form-component.tsx
@@ -95,7 +95,7 @@ const RegisterForm = () => {
                     type="submit" 
                     className="w-full flex justify-center bg-green_1 text-gray-100 p-4 rounded-full tracking-wide font-semibold focus:outline-none hover:bg-green_2 shadow-lg cursor-pointer transition ease-in duration-300"
                 >
-                    {currentUserStatus === "loading" ? <PulseLoader color="#fff" size={16}></PulseLoader> : "Register"}
+                    {currentUserStatus === "loading" ? <PulseLoader color="#fff" size={16}></PulseLoader> : "Sign up!"}
                 </button>
             </form>
 
